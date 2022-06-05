7e8c844446e65f236f40e618ce85b1752340e0df
khalil el kadih <khalil94elkadih@gmail.com>
2022-06-05 19:51:02 +0000

    some update

--- includes/navbar.php
+++ includes/navbar.php
@@ -6,7 +6,7 @@
   <div class="sidebar-brand-icon rotate-n-15">
     <i class="fas fa-laugh-wink"></i>
   </div>
-  <div class="sidebar-brand-text mx-3">FUNDA <sup>WEB IT</sup></div>
+  <div class="sidebar-brand-text mx-3">Gestion du stock</div>
 </a>
 
 <!-- Divider -->
@@ -16,7 +16,7 @@
 <li class="nav-item active">
   <a class="nav-link" href="index.php">
     <i class="fas fa-fw fa-tachometer-alt"></i>
-    <span>Dashboard</span></a>
+    <span>Gestion du stock </span></a>
 </li>
 
 <!-- Divider -->
@@ -31,13 +31,13 @@
 <li class="nav-item">
   <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
     <i class="fas fa-fw fa-cog"></i>
-    <span>Components</span>
+    <span>produit</span>
   </a>
   <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
     <div class="bg-white py-2 collapse-inner rounded">
-      <h6 class="collapse-header">Custom Components:</h6>
-      <a class="collapse-item" href="buttons.html">Buttons</a>
-      <a class="collapse-item" href="cards.html">Cards</a>
+      <h6 class="collapse-header">Custom Product:</h6>
+      <a class="collapse-item" href="buttons.html">Add New Product</a>
+      <a class="collapse-item" href="cards.html">Show All product</a>
     </div>
   </div>
 </li>
