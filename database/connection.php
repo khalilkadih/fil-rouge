8e22407949e5b6e28826b09702f3a58619582ea1
khalil el kadih <khalil94elkadih@gmail.com>
2022-06-11 22:06:03 +0000

    connection to base donner

---
+++ database/connection.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace database;
+
+use PDOException;
+
+class Connection
+{
+
+    static public function Connect()
+    {
+
+        try {
+            $cnx = new \PDO('mysql:host=localhost;dbname=gestion_de_stock', 'root', '');
+            $cnx->exec("set names utf8");
+            $cnx->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
+        }  catch (PDOException $e) {
+            print "Erreur !: " . $e->getMessage() . "<br/>";
+            die();
+        }
+
+
+        return $cnx;
+    }
+}
