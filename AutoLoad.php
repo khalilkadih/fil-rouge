3be683df106c4db38998889cef55f2fa981d9fb5
khalil el kadih <khalil94elkadih@gmail.com>
2022-06-11 22:02:23 +0000

    added autoload

---
+++ AutoLoad.php
@@ -0,0 +1,14 @@
+<?php 
+
+session_start();
+include_once('Constant.php');
+include_once('./app/Redirect.php');
+spl_autoload_register(function($className)
+{
+    $path=str_replace("\\",DIRECTORY_SEPARATOR,$className);
+    $path=$className.'.php';
+    if(is_readable($path))
+    {
+        require_once($path);
+    }
+});
