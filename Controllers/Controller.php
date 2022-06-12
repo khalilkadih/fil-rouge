988c207c8b5dfa1faf6c0fffe23f83e2cbeda9a6
khalil el kadih <khalil94elkadih@gmail.com>
2022-06-12 03:20:05 +0100

    Modifie controller

---
+++ Controllers/Controller.php
@@ -0,0 +1,13 @@
+<?php
+// namespace controllers;
+
+namespace controllers;
+class Controller
+{
+
+    public function view($view_name)
+    {
+        include ('Views/'.$view_name.'.php');
+    }
+    
+}
