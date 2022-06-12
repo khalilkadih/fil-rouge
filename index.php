293211e41529aa2c100850eaa016ce7ffbd34ce8
khalil el kadih <khalil94elkadih@gmail.com>
2022-06-12 03:19:39 +0100

    update root of site

--- index.php
+++ index.php
@@ -6,20 +6,15 @@ require_once('./app/Session.php');
 require_once('./app/session.php');
 use database\Connection;
 use controllers\HomeController;
-
 $home = new HomeController();
-$pages = ['index', 'Achat', 'Client', 'Configuration','dashboard','produit','rapport','utilisateur','vente'];
+$pages = ['index', 'Achat', 'Clients', 'Configuration','dashboard','produit','fournisseurs','rapports','utilisateur','vente','home'];
 
-if (isset($_GET['page'])) {
-    if ((in_array($_GET['page'], $pages))) {
-
-        $page = $_GET['page'];
-
+    $page = str_replace("/pfa/",'',$_SERVER['REQUEST_URI']);
+    if ((in_array($page, $pages))) {
         $home->index($page);
-    } else {
-
+    } else if($page==''){
+           $home->index('index');
+    }else{
+       
         include('views/includes/404.php');
     }
-} else {
-    $home->index('index');
-}
