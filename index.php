7a5566586203aa48dc7093e5e4352b46713abeda
khalil el kadih <khalil94elkadih@gmail.com>
2022-06-13 03:19:36 +0100

    editer sur index

--- index.php
+++ index.php
@@ -7,9 +7,9 @@ require_once('./app/session.php');
 use database\Connection;
 use controllers\HomeController;
 $home = new HomeController();
-$pages = ['index', 'Achat', 'Clients','login', 'Configuration','dashboard','produit','fournisseurs','rapports','utilisateur','vente','home'];
+$pages = ['index', 'Achat', 'Clients','login', 'Configuration','dashboard','produit','fournisseurs','rapports','utilisateur','vente','home','product/edit','product/delete'];
 // $folders=['users_views','produits_views','vente_views','rapports_views','fournisseurs_views','clients_views','configuration_views','dashboard_views'];
-    $page = str_replace("/pfa/",'',$_SERVER['REQUEST_URI']);
+    $page = str_replace("/pfa/",'',getRequestCleanUri());
     if ((in_array($page, $pages))) {
         $home->index($page);
     } else if($page==''){
@@ -17,4 +17,9 @@ $pages = ['index', 'Achat', 'Clients','login', 'Configuration','dashboard','prod
     }else{
        
         include('views/includes/404.php');
+    }
+
+    function getRequestCleanUri(){
+        return parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+
     }
