ef1c137a60f8795450e4849cb302cf534d3b717e
khalil el kadih <khalil94elkadih@gmail.com>
2022-06-13 03:20:32 +0100

    ajouter user controller

--- Controllers/user_controller.php
+++ Controllers/user_controller.php
@@ -1,11 +1,13 @@
 <?php
+namespace controllers;
+use Models\Users_Model;
 
 
 class User_Controller
 {
     public function Get_All_Users()
     {
-        $users=Users::all();
+        $users=Users_Model::Get_All_Users();
         return $users;
 
     }
