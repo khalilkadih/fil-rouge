54b1efe42b1b39130839595b320faced156ec368
khalil el kadih <khalil94elkadih@gmail.com>
2022-06-14 16:52:09 +0100

    create function insert product to db

--- Controllers/Product_Controller.php
+++ Controllers/Product_Controller.php
@@ -4,6 +4,7 @@ namespace controllers;
 use Models\Products_Model;
 use database\Connection;
 use PDOException;
+use Session;
 
 class Product_Controller
 {
@@ -12,6 +13,36 @@ class Product_Controller
         $product=Products_Model::Get_All_Product();
         return $product;
     }
+
+
+
+
+    //insert product
+    public function InserProduct()
+    {
+        if (isset($_POST['insertProduct'])) 
+        {
+           $data = array(
+                'name_product' => $_POST['name_product'],
+                'qte_aviable' => $_POST['qte_aviable'],
+                'prix_achat' => $_POST['prix_achat'],
+                'prix_vente' => $_POST['prix_vente'],
+                'id_categorie' => $_POST['categorie']
+                
+            );
+
+
+            $product = Products_Model::Insert_Product($data);
+            if ($product == 'ok') {
+
+                echo 'Inserted successfully';
+                 Session::Set('succes', 'Product Added Successfully');
+                header('location:' . BASE_URL.'produit');
+            } else {
+                echo "insertion echouée";
+            }
+        }
+    }     
 }
 
 
