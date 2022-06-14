29b38f3912b8f52b8da02cdf6f3238ccf1956cc9
khalil el kadih <khalil94elkadih@gmail.com>
2022-06-14 17:00:20 +0100

    create controller for achat to get all achat

--- Models/Achats_Model.php
+++ Models/Achats_Model.php
@@ -6,7 +6,12 @@ use database\Connection;
 class Achats_Model{
     public static function Get_All_Achat()
     {
-        $cnx=Connection::Connect()->prepare('select * from commande_fournisseur');
+        $cnx=Connection::Connect()->prepare('SELECT commande_fournisseur.id_commande_fournisseur,product.name_product,fournisseur.name_fournisseur,commande_fournisseur.quantite_commande_fournisseur,commande_fournisseur.prix_total_commande_fournisseur,commande_fournisseur.date_commande_fournissuer
+        from commande_fournisseur
+        inner JOIN fournisseur
+        on fournisseur.id_fournisseur=commande_fournisseur.id_fournisseur
+        inner JOIN product
+        on product.id_product=commande_fournisseur.id_product');
         $cnx->execute();
         $achat=$cnx->fetchAll();
         return $achat;
