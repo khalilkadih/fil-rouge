17498b070e3e27195d89e1fb7051e5338135e6e1
khalil el kadih <khalil94elkadih@gmail.com>
2022-06-14 17:34:02 +0100

    some update for page produit and add modal to insert data

--- Views/home.php
+++ Views/home.php
@@ -19,7 +19,7 @@
                     </div>
                 </nav>
                 <div class="container-fluid px-4">
-                    <div class="row my-2 d-flex justify-content-center bg-primary">
+                    <div class="row my-2 d-flex justify-content-center">
                         <div class="col-lg-3 col-md-5 mb-4  ">
                             <div class="p-3 d-flex justify-content-around align-items-center card_student bg-secondary ">
                                 <div >
@@ -63,15 +63,15 @@
                     </div>
                     <div class="container-fluid px-4">
                         <div class="row ">
-
-
-                            <div class="d-flex">
+                            <div class="d-flex mt-4 justify-content-around ">
                                 <canvas id="myChart" style="width:100%;max-width:500px"></canvas>
                                 <canvas id="chart2" style="width:100%;max-width:500px"></canvas>
                             </div>
-                            
+                            <div class="mt-5">
 
+                                <canvas id="Chart" style="width:100%;max-width:600px"></canvas>
 
+                            </div>
 
                         </div>
                     </div>
--- Views/produit.php
+++ Views/produit.php
@@ -1,104 +1,165 @@
 <?php
 
+use Controllers\Categorie_Controller;
 use controllers\Product_Controller;
 
-$product=new Product_Controller();
-$results=$product->Get_All_Product();
-// echo '<pre>';
-// print_r($results);
-// echo '</pre>';
+$product = new Product_Controller();
+$results = $product->Get_All_Product();
+$product->InserProduct();
+//get all categorie
+$categorie = new Categorie_Controller();
+$categories = $categorie->Get_All_Categorie();
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<?php require_once("includes/header.php"); ?>
+<main>
+    <div class="d-flex" id="dashboard">
+        <?php require_once('includes/navigation.php'); ?>
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <div id="page-content-dashboard" class="bt">
+            <nav class="navbar navbar-expand-lg   py-1 px-4 cont ">
+                <div class="d-flex align-items-center">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-    <link rel="stylesheet" href="css/dashboard.css" />
-    <title>Gestion du stock </title>
-</head>
+                    <i class="fa fa-bars me-3 " id="menu-toggle"></i>
+                    <h5>Accueil</h5>
+                </div>
 
-<body>
-    <main>
-        <div class="d-flex" id="dashboard">
-            <?php require_once('includes/navigation.php'); ?>
 
-            <div id="page-content-dashboard" class="bt">
-                <nav class="navbar navbar-expand-lg   py-1 px-4 cont ">
-                    <div class="d-flex align-items-center">
-
-                        <i class="fa fa-bars me-3 " id="menu-toggle"></i>
-                        <h5>Accueil</h5>
+                <div class="navbar-nav ms-auto">
+                    <div class="nav-item ">
+                        <form class="d-flex  justify-content-end mt-3 ">
+                            <input class="form-control me-2 " type="search" placeholder="Search..." aria-label="Search">
+                            <a href="./profile.html" class="mx-3 "> <img src="<?= BASE_URL_WITH_VIEWS ?>/img/user (1).png" class="mx-3 w-75"></a>
+                        </form>
                     </div>
+                </div>
 
-
-                    <div class="navbar-nav ms-auto">
-                        <div class="nav-item ">
-                            <form class="d-flex  justify-content-end mt-3 ">
-                                <input class="form-control me-2 " type="search" placeholder="Search..." aria-label="Search">
-                                <a href="./profile.html" class="mx-3 "> <img src="<?=BASE_URL_WITH_VIEWS?>/img/user (1).png" class="mx-3 w-75"></a>
-
-                            </form>
-                        </div>
-                    </div>
-
-                </nav>
+            </nav>
+            <div class="container-fluid px-4">
                 <div class="container-fluid px-4">
-                    <div class="container-fluid px-4">
-                        <div class="row ">
-                            <div class=" d-flex justify-content-between my-3">
-                                <h1 class="fs-4 ">Liste Produit</h1>
+                    <div class="row ">
+                        <div class=" d-flex justify-content-between my-3">
+                            <h1 class="fs-4 ">Liste Produit</h1>
+                            <div>
+                                <!-- <i class="fas fa-sort mx-3  "></i> -->
+                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalInsertProduct">
+                                    Ajouter un Produit
+                                </button>
+                            </div>
+                        </div>
 
-                            </div>
-                            <div class=" table-responsive-sm table-responsive-md">
-                                <table class="table bg-white rounded shadow-sm align-middle overflow-scroll  table-hover">
-                                    <thead>
+                        <div class=" table-responsive-sm table-responsive-md">
+                            <table class="table bg-white rounded shadow-sm align-middle overflow-scroll  table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Id </th>
+                                        <th>name of product</th>
+                                        <th>Quantite Stocke</th>
+                                        <th>Prix d'achat </th>
+                                        <th>prix de vente</th>
+                                        <th>categorie name</th>
+                                        <th class="text-center"> Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($results as $result) { ?>
                                         <tr>
-                                            <th>Id </th>
-                                            <th>name of product</th>
-                                            <th>Quantite Stocke</th>
-                                            <th>Prix d'achat </th>
-                                            <th>prix de vente</th>
-                                            <th>categorie name</th>
-                                            <th class="text-center"> Action</th>
+                                            <td><?php echo $result['id_product'] ?></td>
+                                            <td><?php echo $result['name_product'] ?></td>
+                                            <td><?= $result['qte_aviable'] ?></td>
+                                            <td><?= $result['prix_achat'] ?></td>
+                                            <td><?= $result['prix_vente'] ?></td>
+                                            <td><?= $result['name_categorie'] ?></td>
+                                            <td class="text-center">
+                                                <a href="<?= BASE_URL_WITH_VIEWS ?>/product/edit?id=<?= $result['id_product'] ?>" class="btn btn-primary">Edit</a>
+                                                <a href="<?= BASE_URL_WITH_VIEWS ?>/product/delete?id=<?= $result['id_product'] ?>" class="btn btn-danger">Delete</a>
+                                            </td>
                                         </tr>
-                                    </thead>
-                                    <tbody>
-                                    <!-- (`id_product`, `name_product`, `qte_aviable`, `prix_achat`, `prix_vente`, `id_categorie`) -->
-                                        <?php foreach ($results as $result) { ?>
-                                            <tr>
-                                                <td><?php echo $result['id_product'] ?></td>
-                                                <td><?php echo $result['name_product'] ?></td>
-                                                <td><?= $result['qte_aviable'] ?></td>
-                                                <td><?= $result['prix_achat'] ?></td>
-                                                <td><?= $result['prix_vente'] ?></td>
-                                                <td><?= $result['name_categorie'] ?></td>
-                                                <td class="text-center">
-                                                    <a href="<?=BASE_URL_WITH_VIEWS?>/product/edit?id=<?=$result['id_product']?>" class="btn btn-primary">Edit</a>
-                                                    <a href="<?=BASE_URL_WITH_VIEWS?>/product/delete?id=<?=$result['id_product']?>" class="btn btn-danger">Delete</a>
-                                                </td>
-                                            </tr>
-                                  
-                                        <?php } ?>
-                                    </tbody>
-                                </table>
-                            </div>
+
+                                    <?php } ?>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
-    
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
 
+    </div>
+    <!-- --------------------------------------------Start Modal-------------------------------------------------- -->
 
+    <div class="modal fade" id="modalInsertProduct" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalInsertProduct">Add Product</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form method="POST" class="needs-validation" novalidate>
+                    <div class="modal-body">
+                        <h1>ajouter un produit</h1>
+                        <div class="form-group">
+                            <label for="name_product">name of product</label>
+                            <input type="text" class="form-control" id="name_product" name="name_product" placeholder="name of product" required>
+                            <div class="invalid-feedback">
+                                please enter name of product
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="qte_aviable">Quantite Stocke</label>
+                            <input type="text" class="form-control" id="qte_aviable" name="qte_aviable" placeholder="Quantite Stocke" required>
+                            <div class="invalid-feedback">
+                                please enter Quantite Stocke
+                            </div>
+                        </div>
 
-    </main>
-    
-    <?php require_once('includes/header.php'); ?>
+                        <div class="form-group">
+                            <label for="prix_achat">Prix d'achat</label>
+                            <input type="text" class="form-control" id="prix_achat" name="prix_achat" placeholder="Prix d'achat" required>
+                            <div class="invalid-feedback">
+                                please enter Prix d'achat
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="prix_vente">Prix de vente</label>
+                            <input type="text" class="form-control" id="prix_vente" name="prix_vente" placeholder="Prix de vente" required>
+                            <div class="invalid-feedback">
+                                please enter Prix de vente
+                            </div>
+                        </div>
+                        <!-- //Normalement on travail avec select ici  -->
+                        <!-- <div class="form-group">
+                            <label for="id_categorie">id categorie</label>
+                            <input type="text" class="form-control" id="id_categorie" name="id_categorie" placeholder="Prix de vente" required>
+                            <div class="invalid-feedback">
+                                please enter Prix de vente
+                            </div>
+                        </div> -->
+                        <div class="form-group">
+                        
+                            <label for="categorie"> chose categorie:</label>
+                                <select class="form-control" id="id_categorie" name="categorie" required>
+                                    <option value="">chose categorie:</option>
+                                    <?php foreach ($categories as $categorie) { ?>
+                                        <option value="<?= $categorie['id_categorie']?>">
+                                             <?= $categorie['name_categorie']?>
+                                        </option>
+                                    <?php } ?>
+                                </select>
+                            
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="submit" name="insertProduct" class="btn btn-primary">Save data</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- --------------------------------------------End Modal-------------------------------------------------- -->
+</main>
+
+<?php require_once('includes/footer.php'); ?>
