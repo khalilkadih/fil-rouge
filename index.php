6c0afa15c00730b1752c5ae39c9dda72e8d55496
khalil el kadih <khalil94elkadih@gmail.com>
2022-06-14 16:43:14 +0100

    same update concern constant and index

--- index.php
+++ index.php
@@ -7,9 +7,11 @@ require_once('./app/session.php');
 use database\Connection;
 use controllers\HomeController;
 $home = new HomeController();
-$pages = ['index', 'Achat', 'Clients','login', 'Configuration','dashboard','produit','fournisseurs','rapports','utilisateur','vente','home','product/edit','product/delete'];
+$pages = ['index', 'Achat', 'Clients','login', 'Configuration','dashboard','produit','fournisseurs','rapports','utilisateur','vente','home','product/edit','product/delete','InsertUser'];
 // $folders=['users_views','produits_views','vente_views','rapports_views','fournisseurs_views','clients_views','configuration_views','dashboard_views'];
     $page = str_replace("/pfa/",'',getRequestCleanUri());
+    // echo $page;
+    // print_r($_GET);
     if ((in_array($page, $pages))) {
         $home->index($page);
     } else if($page==''){
