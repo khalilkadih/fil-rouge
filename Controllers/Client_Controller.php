81ad37dba22ca48f4b006a5553c99d815bff6067
khalil el kadih <khalil94elkadih@gmail.com>
2022-06-14 16:44:16 +0100

    create function to insert client to db

--- Controllers/Client_Controller.php
+++ Controllers/Client_Controller.php
@@ -1,15 +1,39 @@
 <?php
 
 namespace Controllers;
+
 use Models\Clients_Model;
+use Session;
 
 class Client_Controller
 {
 
     public function Get_All_Client()
     {
-        $clients=Clients_Model::Get_All_Client();
+        $clients = Clients_Model::Get_All_Client();
         return $clients;
-        
+    }
+
+    //_____insert client_____//
+
+    public function Insert_Client()
+    {
+        if (isset($_POST['insertClient'])) {
+
+            $data = array(
+                'nom' => $_POST['name_client'],
+                'telephone' => $_POST['telephone_client'],
+                'adress' => $_POST['adress_client'],
+                'observation' => $_POST['observation_client']
+            );
+            print_r($data);
+            $client = Clients_Model::Insert_Client($data);
+            if ($client == 'ok') {
+                Session::Set('succes', 'Client Added Successfully');
+                header('location:' . BASE_URL . 'Clients');
+            } else {
+                echo 'Something Wrong';
+            }
+        }
     }
 }
