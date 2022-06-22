6acb60a80e59ca9c1a6f051b6dbbe5a72d4c7484
khalil el kadih <khalil94elkadih@gmail.com>
2022-06-22 22:26:56 +0100

    ajouter et supp categorie

--- Controllers/Categorie_Controller.php
+++ Controllers/Categorie_Controller.php
@@ -1,15 +1,20 @@
 <?php
 
 namespace Controllers;
+
 use Models\Categories_Model;
 use database\Connection;
+use Redirect\Redirect;
+use Session;
 
 
-class Categorie_Controller{
+
+class Categorie_Controller
+{
 
     public function Get_All_Categorie()
     {
-        $categorie=Categories_Model::Get_All_Categorie();
+        $categorie = Categories_Model::Get_All_Categorie();
         return $categorie;
     }
 
@@ -17,9 +22,50 @@ class Categorie_Controller{
 
     public function Delete_Categorie($id_categorie)
     {
-        if(isset($id_categorie)){
+        if (isset($id_categorie)) {
             Categories_Model::Delete_Categorie($id_categorie);
         }
     }
 
+    //add categorie
+
+    public function InsertCategorie()
+    {
+        if (isset($_POST['categorie_submit'])) {
+            $data = array(
+                'name_categorie' => $_POST['name_categorie'],
+                'description_categorie' => $_POST['description_categorie'],
+                
+            );
+            print_r($data);
+            $user = Categories_Model::InserCategorie($data);
+            if ($user) {
+                // Session::Set('success', 'User Added Successfully');
+                header('location:' . BASE_URL . 'Configuration');
+                header('location:' . getUrlWIthMessage('Configuration', 'categorie added successfully', 'success'));
+            } else {
+                echo "insertion echouée";
+            }
+        }
+    }
+
+    ///delete categorie
+
+    public function DeleteCategorie()
+    {
+        
+        if (isset($_GET['deleteCategorie'])) 
+        {
+            
+            $id_user = $_GET['id_categorie'];
+           if(Categories_Model::DeleteCategorie($id_user))
+           {
+            Session::Set('success', 'categorie Deleted Successfully');
+                Redirect::to(BASE_URL,'Configuration');
+           }
+            else {
+                echo "deleted echouée";
+            }
+        }
+    }
 }
--- Models/Categories_Model.php
+++ Models/Categories_Model.php
@@ -2,15 +2,17 @@
 
 
 namespace Models;
+
 use database\Connection;
 use PDOException;
+
 class Categories_Model
 {
-     static public function Get_All_Categorie()
+    static public function Get_All_Categorie()
     {
-        $cnx=Connection::Connect()->prepare("SELECT * FROM categorie");
+        $cnx = Connection::Connect()->prepare("SELECT * FROM categorie");
         $cnx->execute();
-        $categorie=$cnx->fetchAll();
+        $categorie = $cnx->fetchAll();
         return $categorie;
     }
 
@@ -18,8 +20,36 @@ class Categories_Model
     // _____Delete Categorie_____
     static public function Delete_Categorie($id_categorie)
     {
-        $cnx=Connection::Connect()->prepare("DELETE FROM categorie WHERE id_categorie=:id_categorie");
-        $cnx->bindParam(':id_categorie',$id_categorie);
+        $cnx = Connection::Connect()->prepare("DELETE FROM categorie WHERE id_categorie=:id_categorie");
+        $cnx->bindParam(':id_categorie', $id_categorie);
         $cnx->execute();
+    }
+
+
+    ////insert categorie
+    static public function InserCategorie($data)
+    {
+       
+        $cnx = Connection::Connect()->prepare('INSERT INTO `categorie`(`name_categorie`, `description_categorie`) VALUES(:name_categorie,:description_categorie)');
+        $cnx->bindParam(':name_categorie', $data['name_categorie']);
+        $cnx->bindParam(':description_categorie', $data['description_categorie']);
+        $cnx->execute();
+       
+        return $cnx->rowCount()>0;
+    }
+
+
+
+    ///delet categorie
+
+
+    static public function DeleteCategorie($id_categorie){
+
+        $stmt=Connection::Connect()->prepare('DELETE FROM categorie WHERE id_categorie = :id_categorie');
+        $stmt->bindParam(':id_categorie', $id_categorie);
+        $stmt->execute();
+        return $stmt->rowCount()>0;
+       
+       
     }
 }
